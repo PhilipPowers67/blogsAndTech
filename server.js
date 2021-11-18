576e3e17b516235178658fa1522ceea567c43925
Philip Powers <philip_powers607@hotmail.com>
2021-11-18 13:08:38 -0500

    server starts

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const sequelize = require('./config/connection');
+const path = require("path");
+const express = require("express");
+const helpers = require("./utils/helpers");
+// const routes = require("./controllers");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
+const session = require("express-session");
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const PORT = process.env.PORT || 3001;
+const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
-
-
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+app.use(session(sess));
 
 
 
@@ -17,5 +40,5 @@ const PORT = process.env.PORT || 3001;
 
 //Turns on connection to DB and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now Listening on Port${PORT}`));
-});
+    app.listen(PORT, () => console.log(`Now listening on Port:${PORT}`));
+  });
